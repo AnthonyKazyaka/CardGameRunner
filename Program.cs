059d86fc1fdc5a1dfd8fb059c7988eca5f0ebe7b
Anthony Kazyaka <ask.anthony@hotmail.com>
2021-03-18 20:06:41 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CardGameEngine.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace CardGameRunner
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            return host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                    ConfigureServices(services));
+
+        static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.ConfigureCardGameEngineDependencies();
+        }
+    }
+}
